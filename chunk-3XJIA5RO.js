35d02fa96f891967528d79beff9be45b73eaa904
alejgolop <alejgolop@gmail.com>
2025-02-18 17:32:13 +0100

    s

---
+++ chunk-3XJIA5RO.js
@@ -0,0 +1,1 @@
+import{f as o}from"./chunk-OEHGKLW6.js";import{f as m}from"./chunk-GPHNUKZC.js";var r=m(o());function n(t){return(0,r.default)(t).format("HH:mm:ss DD/MM/YYYY")}function Y(t){return(0,r.default)(t).format("HH.mm.ss DD_MM_YYYY")}export{n as a,Y as b};
